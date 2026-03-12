c64ebab86755a552fbe8bd28f8bb68491a392055
Agnes Iradukunda <kundaaggy@gmail.com>
2026-03-12 20:06:39 +0200

    python basics: exercise-2

--- exercise-2.py
+++ exercise-2.py
@@ -2,3 +2,8 @@
 # TODO: Write a greet function. It takes one argument, a name, and returns Hello, Name!. Use a formatted string.
 # ============================================================================
 
+def greet_function(name):
+    return f"Hello, {name}!"
+
+
+print(greet_function("Kunda"))
