0ce0998127aaccabbc5da88071a01bb6a1f90e52
Agnes Iradukunda <kundaaggy@gmail.com>
2026-03-12 21:03:55 +0200

    python basics: exercise-5

--- exercise-5.py
+++ exercise-5.py
@@ -1,6 +1,14 @@
 # ============================================================================
-# TODO: Use a For Loop with a Range 
+# TODO: Use a For Loop with a Range
 
-#Create a function called repeat. 
+# Create a function called repeat.
 # It takes two parameters, a string and a count, and returns a new string that is the old one repeated count times.
 # ============================================================================
+def repeat(string, count):
+    result = ""
+    for i in range(count):
+        result += string
+    return result
+
+
+print(repeat("Hello ", 3))
